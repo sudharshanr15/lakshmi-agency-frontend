bf6c80492aefd95fa65de9413cd662e538cdc0f0
Esakkiraja <esakkiraja100116@gmail.com>
2023-07-30 15:58:14 +0530

    outstanding amount ui fix

--- src/container/amount/outstandAmount.js
+++ src/container/amount/outstandAmount.js
@@ -1,4 +1,60 @@
 export function Payment() {
+  const orders = [
+    {
+      orderedOn: "22May 2022",
+      amount: "₹2100",
+      totalItems: 3,
+      totalAmount: "₹18,400 + ₹200(delivery fee)",
+      orderId: "AISR7234351",
+      dueDate: "24 Jan'23",
+    },
+    {
+      orderedOn: "22May 2022",
+      amount: "₹2100",
+      totalItems: 3,
+      totalAmount: "₹18,400 + ₹200(delivery fee)",
+      orderId: "AISR7234351",
+      dueDate: "24 Jan'23",
+    },
+    {
+      orderedOn: "22May 2022",
+      amount: "₹2100",
+      totalItems: 3,
+      totalAmount: "₹18,400 + ₹200(delivery fee)",
+      orderId: "AISR7234351",
+      dueDate: "24 Jan'23",
+    },
+    {
+      orderedOn: "22May 2022",
+      amount: "₹2100",
+      totalItems: 3,
+      totalAmount: "₹18,400 + ₹200(delivery fee)",
+      orderId: "AISR7234351",
+      dueDate: "24 Jan'23",
+    },
+  ];
+  const orderElements = orders.map((order, index) => (
+    <div className="border rounded-lg mt-2" key={index}>
+      <div className="flex justify-between m-3 md:m-5">
+        <h1 className="text-[#0a4e71] text-sm md:text-1xl font-semibold">
+          Ordered On {order.orderedOn}
+        </h1>
+        <span>{order.amount}</span>
+      </div>
+      <div className="flex text-[#b9babc] justify-between m-3 mt-2  md:m-5">
+        <h1 className="text-sm md:text-lg">Total items : {order.totalItems}</h1>
+        <span className="text-sm md:text-lg">{order.totalAmount}</span>
+      </div>
+      <div className="flex justify-between m-3 mt-2  md:m-5">
+        <h1 className="mt-1 text-sm md:text-lg"> Order ID : {order.orderId}</h1>
+        <span className="bg-[#f9ece4] rounded-md">
+          <p className=" text-[#fd3130] p-2 md:p-3 text-sm md:text-1xl w-auto text-center font-medium rounded-xl">
+            Due on : {order.dueDate}
+          </p>
+        </span>
+      </div>
+    </div>
+  ));
   return (
     <div className="lg:mt-40 ">
       <div className="lg:mx-24 mx-6  p-5 rounded-md">
@@ -33,279 +89,8 @@ export function Payment() {
         </div>
       </div>
 
-      <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 w-auto mx-2 gap-2 lg:gap-2 md:mx-auto justify-between lg:mx-20 mt-4  p-5 rounded-md mb-32">
-        <div className="border rounded-lg mt-2">
-          <div className="flex justify-between p-4 mt-2">
-            <div className="ml-4">
-              <h1 className="text-[#0a4e71] font-bold text-2-xl">
-                Ordered On 22 May 2022
-              </h1>
-              <div className="">
-                <p className="mt-2  text-[#b9babc] md:text-1xl flex-end">
-                  Total items 3
-                </p>
-              </div>
-              <div className="flex">
-                <p className="mt-2  text-black md:text-1xl ">
-                  Order ID : AISR7234351
-                </p>
-              </div>
-            </div>
-            <div className="ml-4 justify-end">
-              <h1 className="text-[#0a4e71] font-bold text-2-xl text-right">
-                ₹21,400
-              </h1>
-              <div className="">
-                <p className="mt-2  text-[#b9babc] md:text-1xl text-right">
-                  ₹18,400 + ₹200(delivery fee)
-                </p>
-                <span className="text-right ">
-                  <p className="mt-2  bg-[#f9ece4] text-[#fd3130] md:text-1xl p-1 w-auto text-center rounded-xl font-semibold">
-                    Due on : 24 Jan'23
-                  </p>
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="border rounded-lg mt-2">
-          <div className="flex justify-between p-4 mt-2">
-            <div className="ml-4">
-              <h1 className="text-[#0a4e71] font-bold text-2-xl">
-                Ordered On 22 May 2022
-              </h1>
-              <div className="">
-                <p className="mt-2  text-[#b9babc] md:text-1xl flex-end">
-                  Total items 3
-                </p>
-              </div>
-              <div className="flex">
-                <p className="mt-2  text-black md:text-1xl ">
-                  Order ID : AISR7234351
-                </p>
-              </div>
-            </div>
-            <div className="ml-4 justify-end">
-              <h1 className="text-[#0a4e71] font-bold text-2-xl text-right">
-                ₹21,400
-              </h1>
-              <div className="">
-                <p className="mt-2  text-[#b9babc] md:text-1xl text-right">
-                  ₹18,400 + ₹200(delivery fee)
-                </p>
-                <span className="text-right ">
-                  <p className="mt-2  bg-[#f9ece4] text-[#fd3130] md:text-1xl p-1 w-auto text-center rounded-xl font-semibold">
-                    Due on : 24 Jan'23
-                  </p>
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="border rounded-lg mt-2">
-          <div className="flex justify-between p-4 mt-2">
-            <div className="ml-4">
-              <h1 className="text-[#0a4e71] font-bold text-2-xl">
-                Ordered On 22 May 2022
-              </h1>
-              <div className="">
-                <p className="mt-2  text-[#b9babc] md:text-1xl flex-end">
-                  Total items 3
-                </p>
-              </div>
-              <div className="flex">
-                <p className="mt-2  text-black md:text-1xl ">
-                  Order ID : AISR7234351
-                </p>
-              </div>
-            </div>
-            <div className="ml-4 justify-end">
-              <h1 className="text-[#0a4e71] font-bold text-2-xl text-right">
-                ₹21,400
-              </h1>
-              <div className="">
-                <p className="mt-2  text-[#b9babc] md:text-1xl text-right">
-                  ₹18,400 + ₹200(delivery fee)
-                </p>
-                <span className="text-right ">
-                  <p className="mt-2  bg-[#f9ece4] text-[#fd3130] md:text-1xl p-1 w-auto text-center rounded-xl font-semibold">
-                    Due on : 24 Jan'23
-                  </p>
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="border rounded-lg mt-2">
-          <div className="flex justify-between p-4 mt-2">
-            <div className="ml-4">
-              <h1 className="text-[#0a4e71] font-bold text-2-xl">
-                Ordered On 22 May 2022
-              </h1>
-              <div className="">
-                <p className="mt-2  text-[#b9babc] md:text-1xl flex-end">
-                  Total items 3
-                </p>
-              </div>
-              <div className="flex">
-                <p className="mt-2  text-black md:text-1xl ">
-                  Order ID : AISR7234351
-                </p>
-              </div>
-            </div>
-            <div className="ml-4 justify-end">
-              <h1 className="text-[#0a4e71] font-bold text-2-xl text-right">
-                ₹21,400
-              </h1>
-              <div className="">
-                <p className="mt-2  text-[#b9babc] md:text-1xl text-right">
-                  ₹18,400 + ₹200(delivery fee)
-                </p>
-                <span className="text-right ">
-                  <p className="mt-2  bg-[#f9ece4] text-[#fd3130] md:text-1xl p-1 w-auto text-center rounded-xl font-semibold">
-                    Due on : 24 Jan'23
-                  </p>
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="border rounded-lg mt-2">
-          <div className="flex justify-between p-4 mt-2">
-            <div className="ml-4">
-              <h1 className="text-[#0a4e71] font-bold text-2-xl">
-                Ordered On 22 May 2022
-              </h1>
-              <div className="">
-                <p className="mt-2  text-[#b9babc] md:text-1xl flex-end">
-                  Total items 3
-                </p>
-              </div>
-              <div className="flex">
-                <p className="mt-2  text-black md:text-1xl ">
-                  Order ID : AISR7234351
-                </p>
-              </div>
-            </div>
-            <div className="ml-4 justify-end">
-              <h1 className="text-[#0a4e71] font-bold text-2-xl text-right">
-                ₹21,400
-              </h1>
-              <div className="">
-                <p className="mt-2  text-[#b9babc] md:text-1xl text-right">
-                  ₹18,400 + ₹200(delivery fee)
-                </p>
-                <span className="text-right ">
-                  <p className="mt-2  bg-[#f9ece4] text-[#fd3130] md:text-1xl p-1 w-auto text-center rounded-xl font-semibold">
-                    Due on : 24 Jan'23
-                  </p>
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="border rounded-lg mt-2">
-          <div className="flex justify-between p-4 mt-2">
-            <div className="ml-4">
-              <h1 className="text-[#0a4e71] font-bold text-2-xl">
-                Ordered On 22 May 2022
-              </h1>
-              <div className="">
-                <p className="mt-2  text-[#b9babc] md:text-1xl flex-end">
-                  Total items 3
-                </p>
-              </div>
-              <div className="flex">
-                <p className="mt-2  text-black md:text-1xl ">
-                  Order ID : AISR7234351
-                </p>
-              </div>
-            </div>
-            <div className="ml-4 justify-end">
-              <h1 className="text-[#0a4e71] font-bold text-2-xl text-right">
-                ₹21,400
-              </h1>
-              <div className="">
-                <p className="mt-2  text-[#b9babc] md:text-1xl text-right">
-                  ₹18,400 + ₹200(delivery fee)
-                </p>
-                <span className="text-right ">
-                  <p className="mt-2  bg-[#f9ece4] text-[#fd3130] md:text-1xl p-1 w-auto text-center rounded-xl font-semibold">
-                    Due on : 24 Jan'23
-                  </p>
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="border rounded-lg mt-2">
-          <div className="flex justify-between p-4 mt-2">
-            <div className="ml-4">
-              <h1 className="text-[#0a4e71] font-bold text-2-xl">
-                Ordered On 22 May 2022
-              </h1>
-              <div className="">
-                <p className="mt-2  text-[#b9babc] md:text-1xl flex-end">
-                  Total items 3
-                </p>
-              </div>
-              <div className="flex">
-                <p className="mt-2  text-black md:text-1xl ">
-                  Order ID : AISR7234351
-                </p>
-              </div>
-            </div>
-            <div className="ml-4 justify-end">
-              <h1 className="text-[#0a4e71] font-bold text-2-xl text-right">
-                ₹21,400
-              </h1>
-              <div className="">
-                <p className="mt-2  text-[#b9babc] md:text-1xl text-right">
-                  ₹18,400 + ₹200(delivery fee)
-                </p>
-                <span className="text-right ">
-                  <p className="mt-2  bg-[#f9ece4] text-[#fd3130] md:text-1xl p-1 w-auto text-center rounded-xl font-semibold">
-                    Due on : 24 Jan'23
-                  </p>
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="border rounded-lg mt-2">
-          <div className="flex justify-between p-4 mt-2">
-            <div className="ml-4">
-              <h1 className="text-[#0a4e71] font-bold text-2-xl">
-                Ordered On 22 May 2022
-              </h1>
-              <div className="">
-                <p className="mt-2  text-[#b9babc] md:text-1xl flex-end">
-                  Total items 3
-                </p>
-              </div>
-              <div className="flex">
-                <p className="mt-2  text-black md:text-1xl ">
-                  Order ID : AISR7234351
-                </p>
-              </div>
-            </div>
-            <div className="ml-4 justify-end">
-              <h1 className="text-[#0a4e71] font-bold text-2-xl text-right">
-                ₹21,400
-              </h1>
-              <div className="">
-                <p className="mt-2  text-[#b9babc] md:text-1xl text-right">
-                  ₹18,400 + ₹200(delivery fee)
-                </p>
-                <span className="text-right ">
-                  <p className="mt-2  bg-[#f9ece4] text-[#fd3130] md:text-1xl p-1 w-auto text-center rounded-xl font-semibold">
-                    Due on : 24 Jan'23
-                  </p>
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 w-auto mx-2 gap-2 lg:gap-2 md:mx-auto justify-between lg:mx-20  p-5 rounded-md mb-32">
+        {orderElements}
       </div>
     </div>
   );
