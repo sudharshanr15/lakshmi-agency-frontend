f1b0f507e08532ee7a39f05c9b994c1dd51713e8
p-karthikeyan <keyan4676@gmail.com>
2023-08-07 17:22:29 +0530

    pvc page integrated, yet to fix some issues

---
+++ src/app/pvc/page.js
@@ -0,0 +1,15 @@
+import "../globals.css"
+import { Nav } from "@/components/"
+import { Total,Example } from "@/container/pvc"
+
+const Page = () => {
+  return (
+    <div>
+      <Nav/>
+      <Total/>
+      <Example/>
+    </div>
+  )
+}
+
+export default Page
---
+++ src/container/pvc/index.js
@@ -0,0 +1,2 @@
+export {default as Example} from './filter'
+export { default as Total } from './total'
