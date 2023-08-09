ba00912da1bd299ef2db41e7cd125d6e43ad6f47
Esakkiraja <esakkiraja100116@gmail.com>
2023-08-09 20:11:45 +0530

    fix : ui card change

--- src/container/product/content.js
+++ src/container/product/content.js
@@ -2,7 +2,7 @@ import Card from "./card";
 
 const Content = () => {
   return (
-    <div className=" m-5 lg:m-10 pb-80 md:pb-64 lg:pb-0 grid grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-10">
+    <div className=" m-5 lg:m-10 pb-80 md:pb-64 lg:pb-0 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-10">
       <Card />
       <Card />
       <Card />
