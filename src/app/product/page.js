7fbc4f2532550e374bf705e951e8d00575232c9a
Esakkiraja <esakkiraja100116@gmail.com>
2023-08-09 19:48:56 +0530

    product page

---
+++ src/app/product/page.js
@@ -0,0 +1,14 @@
+"use client";
+import "../globals.css";
+import { Nav, Footer } from "@/components/";
+import { List } from "@/container/product";
+
+export default function Page() {
+  return (
+    <>
+      <Nav />
+      <List />
+      <Footer />
+    </>
+  );
+}
