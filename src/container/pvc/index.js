6fc01719452234eea9a1370315fcca481403eb71
Esakkiraja <esakkiraja100116@gmail.com>
2023-08-14 11:39:25 +0530

    fix : pvc error fix

--- src/container/pvc/index.js
+++ src/container/pvc/index.js
@@ -1,4 +1,4 @@
-export {default as Example} from './filter'
-export { default as Total } from './total'
-export { default as Mobilenav} from './mobilenav' 
-export {default as Sort} from './sort'
+export { default as Example } from "./filter";
+export { default as Total } from "./total";
+export { default as Mobilenav } from "./mobilenav";
+export { default as Sort } from "./sort";
