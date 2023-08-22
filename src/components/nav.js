8e140b7a6496a56989807ecd3af55e2d74a6d539
GSNadarajan <nadarajangs31@gmail.com>
2023-08-22 16:17:49 +0530

    profile module

--- src/components/nav.js
+++ src/components/nav.js
@@ -541,6 +541,12 @@ export function Nav() {
       current: false,
       pincode: "622023",
       icon: (
+        <button
+          className=""
+          type="button"
+          // onClick={toggleProfile}
+          aria-controls="drawer-navigation"
+        >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -555,6 +561,7 @@ export function Nav() {
             d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
           />
         </svg>
+        </button>
       ),
     },
   ];
@@ -891,7 +898,9 @@ export function Nav() {
                       />
                     </svg>
 
-                    <span className="mr-7 text-white ">John Doe</span>
+                    <button type="button" className="mr-7 text-white ">
+                      <a href="/profile" >John Doe</a>
+                    </button>
                   </div>
                 </div>
 
