1215b00e22439fe2102e143ae45033bd7b24c635
GSNadarajan <nadarajangs31@gmail.com>
2023-10-09 17:32:34 +0530

    feat : Updated routing

--- src/app/product/[...slug]/page.js
+++ src/app/product/[...slug]/page.js
@@ -1,8 +1,16 @@
+function reverseFormatText(formattedText) {
+  return formattedText
+    .split('_')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+}
+
 export default function Page({ params }) {
   const { slug } = params;
 
-  const ChoosedCategory = slug[0];
-  const subcategory = slug[1];
+  const ChoosedCategory = reverseFormatText(slug[0]);
+  const subcategory = reverseFormatText(slug[1]);
+  
 
   return (
     <div className="mt-5 mx-5">
