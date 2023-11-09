6acf272d892e40e39c69ec8f3a7907dd861a625d
esakkiraja100116 <esakkiraja100116@gmail.com>
2023-11-09 14:54:18 +0530

    feat: signup page ui added

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,12 @@
 "use client";
 import "./globals.css";
 import React, { useEffect, useState } from "react";
+import { SignUp } from "@/container/signUp";
 
 export default function Main() {
   return (
     <>
-     <h1>
-        hello world
-      </h1>
+      <SignUp />
     </>
   );
 }
