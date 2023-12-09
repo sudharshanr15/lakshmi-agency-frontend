622fb8b254da9ebce1d8010d38c295dffab09e1d
esakkiraja100116 <esakkiraja100116@gmail.com>
2023-12-09 13:32:40 +0530

    feat: filter page added pvc route

---
+++ src/container/pvc/index.js
@@ -0,0 +1,4 @@
+export { default as Example } from "./filter";
+export { default as Total } from "./total";
+export { default as Mobilenav } from "./mobilenav";
+export { default as Sort } from "./sort";
