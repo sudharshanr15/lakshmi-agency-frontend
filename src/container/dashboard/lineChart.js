c836a59ebf8b64679bc527219fee13d06b477f8b
MohammedThalha2002 <mohammedalithalha2002@gmail.com>
2024-01-20 10:37:56 +0530

    ApexCharts - window refernce error resolved

--- src/container/dashboard/lineChart.js
+++ src/container/dashboard/lineChart.js
@@ -1,8 +1,8 @@
 "use client";
-
 import { getChartData } from "@/controller/dashboardController";
 import { useEffect, useState } from "react";
-import Chart from "react-apexcharts";
+import dynamic from "next/dynamic";
+const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
 
 export function LineChart() {
   const [chatData, setChartData] = useState(null);
