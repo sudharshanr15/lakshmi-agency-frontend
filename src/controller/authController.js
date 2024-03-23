281782946d41b623fb206e394702aec37fbff1ab
Anitha <anitha@iweensoft.com>
2024-03-23 14:43:44 +0530

    api configuration on login

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -2,7 +2,7 @@ import { showToast } from "@/utils/showToast";
 import axios from "axios";
 import { toast } from "react-toastify";
 
-const baseUrl = "https://test01.lakshmiagency.com/api/method";
+const baseUrl = "http://64.227.146.248//api/method";
 
 let config = {
   method: "",
@@ -42,28 +42,41 @@ export const signUp = async (body) => {
 export const login = async (body, setTempID, setAuthState) => {
   config.method = "POST";
   config.data = body;
-  config.url = baseUrl + "/libnext.auth.login";
-  // console.log(config);
+  config.url = baseUrl + "/lakshmiagency.api.v1.otp.send_otp";
+  console.log(config);
 
-  axios
-    .request(config)
-    .then((res) => {
-      const data = res.data.data;
-      console.log(data);
-      setTempID(data.temp_id);
-      console.log(res.data.message);
-      setAuthState("OTP");
-      //  GO TO OTP PAGE
-    })
-    .catch((err) => {
-      console.log("Login failed");
-      console.log(err);
-      showToast(
-        toast,
-        "error",
-        err.response.data.message || "Something went wrong"
-      );
-    });
+  let mobile = body.user_id.split("");
+  mobile.shift();
+  mobile.shift();
+  console.log(mobile.join(""));
+
+  let response = await axios.post(
+    `${baseUrl}/lakshmiagency.api.v1.otp.send_otp?mobile_no=${mobile.join("")}`
+  );
+  // setTempID(response.data.data.temp_id);
+  // console.log(response.data.data.message);
+  setAuthState("OTP");
+  console.log("response", response);
+
+  // axios
+  //   .request(config)
+  //   .then((res) => {
+  //     const data = res.data.data;
+  //     console.log(data);
+  //     setTempID(data.temp_id);
+  //     console.log(res.data.message);
+  //     setAuthState("OTP");
+  //     //  GO TO OTP PAGE
+  //   })
+  //   .catch((err) => {
+  //     console.log("Login failed");
+  //     console.log(err);
+  //     showToast(
+  //       toast,
+  //       "error",
+  //       err.response.data.message || "Something went wrong"
+  //     );
+  //   });
 };
 
 // OTP
@@ -71,30 +84,52 @@ export const OTPSubmit = async (body, router) => {
   config.method = "POST";
   config.data = body;
   config.url = baseUrl + "/libnext.auth.verify";
+
+  let mobile = body.user_id.split("");
+  mobile.shift();
+  mobile.shift();
+  console.log(mobile.join(""));
+
+  let response = await axios.post(
+    `${baseUrl}/lakshmiagency.api.v1.auth.authenticate?mobile_no=${mobile.join(
+      ""
+    )}&otp=${body.otp}&session_expiry=18000`
+  );
+  // setTempID(response.data.data.temp_id);
+  // console.log(response.data.data.message);
+  // setAuthState("OTP");
+  if (response.data && response.data.message.access_token) {
+    console.log("test");
+    router.push("/dashboard");
+  } else {
+    showToast(toast, "error", response.data.message || "Something went wrong");
+  }
+  console.log("response", response);
+
   // console.log(config);
   // refreshToken();
 
-  axios
-    .request(config)
-    .then((res) => {
-      const data = res.data.data;
-      // console.log(data);
-      const accessToken = data.access_token;
-      const refreshToken = data.refresh_token;
-      sessionStorage.setItem("access_token", accessToken);
-      sessionStorage.setItem("refresh_token", refreshToken);
+  // axios
+  //   .request(config)
+  //   .then((res) => {
+  //     const data = res.data.data;
+  //     // console.log(data);
+  //     const accessToken = data.access_token;
+  //     const refreshToken = data.refresh_token;
+  //     sessionStorage.setItem("access_token", accessToken);
+  //     sessionStorage.setItem("refresh_token", refreshToken);
 
-      //  GO TO HOMEPAGE PAGE
-      router.push("/dashboard");
-    })
-    .catch((err) => {
-      console.log(err);
-      showToast(
-        toast,
-        "error",
-        err.response.data.message || "Something went wrong"
-      );
-    });
+  //     //  GO TO HOMEPAGE PAGE
+  //     router.push("/dashboard");
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //     showToast(
+  //       toast,
+  //       "error",
+  //       err.response.data.message || "Something went wrong"
+  //     );
+  //   });
 };
 
 export const refreshToken = async () => {
